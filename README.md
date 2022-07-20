a65fcb3f0e8460d551c08c9ff1f5506e5a306eba
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-07-20 22:49:52 +0200

    feat: use dagger binary to run lsp (#8)
    Take in consideration Sam's comment to directly embbed LSP in
    Dagger binary.
    
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ A VSCode extension so use [Dagger CUE LSP](https://github.com/dagger/daggerlsp).
 
 For more information about the LSP, directly go to [daggercuelsp](https://github.com/dagger/daggerlsp).
 
-### Usage
+### Install LSP
 
-You will  need to install `daggerlsp` binary to use this extension.  
-Please, go to this [installation guide](https://github.com/dagger/daggerlsp#releases).
+Dagger LSP is already embedded in Dagger binary :rocket:
+
+If you do not have already installed dagger, go to [install guide](https://docs.dagger.io/install)
--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,39 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { worker } from "cluster";
-import * as vscode from "vscode";
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-} from "vscode-languageclient/node";
+import { worker } from 'cluster';
+import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
-const clientId = "daggerlsp";
-const outputChannel = vscode.window.createOutputChannel("logs", "cue"); // output channel for debug
+const clientId = 'daggerlsp';
+const outputChannel = vscode.window.createOutputChannel('logs', 'cue'); // output channel for debug
 let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-  vscode.window.showInformationMessage("Activation of the extension");
-
   // Restart language server on command palette
-  let restartCmd = vscode.commands.registerCommand(
-    `${clientId}.restart`,
-    async () => {
-      outputChannel.appendLine("Restart command triggered");
-      await stopClient();
-      startClient(context);
-    }
-  );
+  let restartCmd = vscode.commands.registerCommand(`${clientId}.restart`, async () => {
+    outputChannel.appendLine('Restart command triggered');
+    await stopClient();
+    startClient(context);
+  });
 
   // Show logs on command palette
-  let showLogsCmd = vscode.commands.registerCommand(
-    `${clientId}.showLogs`,
-    () => {
-      outputChannel.appendLine("Show logs triggered");
-      if (!client) {
-        return;
-      }
-      client.outputChannel.show(true);
+  let showLogsCmd = vscode.commands.registerCommand(`${clientId}.showLogs`, () => {
+    outputChannel.appendLine('Show logs triggered');
+    if (!client) {
+      return;
     }
-  );
+    client.outputChannel.show(true);
+  });
 
   //// Debug purposes atm, remove it when cue files get properly loaded ////
   let start = vscode.commands.registerCommand(`${clientId}.start`, () => {
-    outputChannel.appendLine("Manual activation of extention");
+    outputChannel.appendLine('Manual activation of extention');
   });
 
   context.subscriptions.push(restartCmd, showLogsCmd, start);
 
-  vscode.window.showInformationMessage("Extension activated");
   outputChannel.show();
 
   startClient(context);
@@ -54,22 +41,21 @@ export async function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export async function deactivate() {
-  outputChannel.appendLine("Extension deactivated");
   await stopClient();
 }
 
 export async function startClient(context: vscode.ExtensionContext) {
   let serverOptions: ServerOptions = {
-    run: { command: "daggerlsp" }, // debug for small repro
-    debug: { command: "daggerlsp" }, // not working yet
+    run: { command: 'dagger', args: ['lsp'] }, // debug for small repro
+    debug: { command: 'dagger', args: ['lsp'] }, // not working yet
   };
 
   // Corresponding docs: https://code.visualstudio.com/api/references/document-selector
   let clientOptions: LanguageClientOptions = {
     // Register the server for cue documents
     documentSelector: [
-      { scheme: "file", language: "cue" },
-      { scheme: "untitled", language: "cue" },
+      { scheme: 'file', language: 'cue' },
+      { scheme: 'untitled', language: 'cue' },
     ],
   };
 
@@ -86,35 +72,15 @@ export async function startClient(context: vscode.ExtensionContext) {
   // https://github.com/microsoft/vscode-languageserver-node/blame/f97bb73dbfb920af4bc8c13ecdcdc16359cdeda6/README.md#L41-L43
   // fixes race condition
   client.registerProposedFeatures();
-  console.log("start client");
-  await client.start();
-  console.log("client started");
 
-  const res = await exploreWorkspace("/private/tmp/test");
-
-  res.map(([fileName, type]) => {
-    console.log(fileName);
-  });
-}
-
-async function exploreWorkspace(path: string) {
-  console.log(path);
-  const res = (
-    await vscode.workspace.fs.readDirectory(vscode.Uri.file(path))
-  ).map(([name, type]) => [`${path}/${name}`, type]);
-
-  let items = res;
-  await Promise.all(
-    res.map(async ([name, type]) => {
-      // If folder
-      if (type === 2) {
-        const newItems = await exploreWorkspace(name as string);
-        items.push(...newItems);
-      }
-    })
-  );
-
-  return items;
+  try {
+    await client.start();
+  } catch (e: any) {
+    outputChannel.clear();
+    outputChannel.appendLine(
+      'Could not start Dagger LSP.\nPlease make sure you have install Dagger CLI or go to https://docs.dagger.io/install to install it.'
+    );
+  }
 }
 
 async function stopClient() {
